c46874e91a8327983b1f18bc4945ccd00573fe4e
u9g <git@u9g.dev>
2022-06-06 03:57:48 -0400

    Update downloader.sh

--- .github/workflows/downloader.sh
+++ .github/workflows/downloader.sh
@@ -5,8 +5,7 @@ then
   for ver in "1.7" "1.8" "1.8.9" "1.9.4" "1.10.2" "1.11.2" "1.12.2" "1.13.2"; do
     wget https://raw.githubusercontent.com/u9g/repopublisher/main/.github/workflows/fixgradle.js
     node fixgradle.js $MAVEN_USERNAME $MAVEN_PASSWORD
-    ./gradlew publish
-    echo $ver
+    python ./yarn.py $ver publish
   done
 else
   git switch -f $BRANCH
