e3defb64a871b9fc2035487f9e5dd086f12873f2
u9g <git@u9g.dev>
2022-09-11 10:28:24 -0400

    Update downloader.sh

--- .github/workflows/downloader.sh
+++ .github/workflows/downloader.sh
@@ -1,7 +1,8 @@
 git clone https://github.com/$REPO code
 cd code
 git switch -f $BRANCH
-if [ $REPO == *yarn ]; then
+if [[ $REPO == *yarn ]]
+then
   wget https://raw.githubusercontent.com/u9g/repopublisher/main/.github/workflows/fixgradle.js
   for ver in "1.7" "1.8" "1.8.9" "1.9.4" "1.10.2" "1.11.2" "1.12.2" "1.13.2"; do
     node fixgradle.js $MAVEN_USERNAME $MAVEN_PASSWORD
